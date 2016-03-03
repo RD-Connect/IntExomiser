cc93438ba6b80134ce0d29ea5a2e9eaeee04f57c
Anastasios Papakonstantinou <taspapak@hotmail.com>
2016-03-03 16:06:58 +0100

    Update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.call("gunicorn --reload --worker-class gevent --workers 9 -b 172.16.20.56:8082 api:app",shell=True)
+subprocess.call("gunicorn --reload --worker-class gevent --workers 9 -b localhost:8082 api:app",shell=True)
