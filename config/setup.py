daa324e2a44a486c6a39b60c2fe36fbdb83f503a
taspapak <taspapak@hotmail.com>
2016-05-30 12:58:24 +0200

    dependencies

--- config/setup.py
+++ config/setup.py
@@ -6,6 +6,7 @@ setup(
     install_requires=[
         "flask == 0.10.1",
         "flask-restful == 0.3.5",
+        "Flask-CAS == 1.0.0",
         "Flask-Cors == 2.1.2",
         "requests == 2.9.1",
         "gunicorn == 19.4.5",
--- gunicorn.py
+++ gunicorn.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.call("gunicorn --reload --worker-class gevent --workers 9 -b localhost:8082 api:app",shell=True)
+subprocess.call("gunicorn --reload --worker-class gevent --workers 9 -b 0.0.0.0:8082 api:app",shell=True)
